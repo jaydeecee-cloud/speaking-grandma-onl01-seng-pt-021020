b2135c96a8eace9b647fddf23ae03a1b442f3d05
Joseph Choi <jdc07@live.com>
2020-02-14 23:39:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,11 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if (phrase == phrase.downcase)
+  if(phrase == "I LOVE YOU GRANDMA!")
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif (phrase == phrase.downcase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif (phrase == phrase.upcase)
     return "NO, NOT SINCE 1938!"
-  elsif (phrase == "I LOVE YOU GRANDMA!")
-    return "I LOVE YOU TOO PUMPKIN!"
   end
 end
